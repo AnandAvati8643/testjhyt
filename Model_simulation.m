a835a3fdf71752e72647d2d80c253c2af496bf37
39216 <39216@39216-445824.TELXSIDS.NET>
2024-03-21 15:45:51 +0530

    Adding to git repo

---
+++ Model_simulation.m
@@ -0,0 +1,23 @@
+% f='TRAnsfer_Function'
+f=input("enter the model file name");
+open_system(f)
+% if(is_simulink_loaded)
+%     fprintf("loaded successfully\n");
+% end
+is_loaded = bdIsLoaded(f);
+if(is_loaded)
+     fprintf("Model is loaded successfully\n ");
+% else
+%       disp("Model is not successfull");
+end
+ 
+  simOut=sim(f);
+set_param(f,"SimulationCommand","start");
+% if simOut.get('Error') == 0
+%     disp("No errors");
+% end
+if isempty(simOut)
+        disp('No simulation results found. Check for errors or warnings.');
+else
+    disp('Simulation completed successfully.');
+end
